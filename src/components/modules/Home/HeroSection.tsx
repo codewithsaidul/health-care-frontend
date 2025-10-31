fa7f6a85cd2cd5c2576013651e87f48ec03ee365
codewithsaidul <codewithsaidul@gmail.com>
2025-10-31 12:44:39 +0600

    refectorh hero section

--- src/components/modules/Home/HeroSection.tsx
+++ src/components/modules/Home/HeroSection.tsx
@@ -7,8 +7,9 @@ export function HeroSection() {
   return (
     <section className="relative bg-linear-to-br from-primary/5 via-background to-accent/5 py-20 md:py-32 overflow-hidden">
       {/* Decorative elements */}
-      <div className="absolute top-0 right-0 w-96 h-96 bg-primary/10 rounded-full blur-3xl -z-10" />
-      <div className="absolute bottom-0 left-0 w-96 h-96 bg-accent/10 rounded-full blur-3xl -z-10" />
+      <div className="absolute top-0 left-0 w-96 h-96 bg-accent/50 rounded-full blur-3xl -z-10" />
+      <div className="absolute bottom-0 right-0 w-96 h-96 bg-primary/50 rounded-full blur-3xl -z-10" />
+      {/* <div className="absolute top-0 left-0 w-96 h-96 bg-accent/50 rounded-full blur-3xl! -z-10" /> */}
 
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
